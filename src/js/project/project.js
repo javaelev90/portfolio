365a63b6915f6192baf73a08b2647794809d9ce2
Anders <37899614+javaelev90@users.noreply.github.com>
2022-03-14 23:09:51 +0100

    Added final SL project to portfolio page

---
+++ src/js/project/project.js
@@ -0,0 +1,33 @@
+// Removes game iframe overlay and sets the iframe src
+function runGame() {
+    var iframe = document.getElementById("game-window");
+    var iframeOverlay = document.getElementById("iframe-overlay");
+    iframe.src = iframe.dataset.src; 
+    iframeOverlay.style.display = "none";
+    iframe.style.display = "flex";
+}
+
+// Sets the game-window iframe to fullscreen if allowed
+function iframeFullscreen() {
+    // Check if browser allows fullscreen
+    if (document.fullscreenEnabled          || 
+        document.webkitFullscreenEnabled    || 
+        document.mozFullScreenEnabled       ||
+        document.msFullscreenEnabled        ) {
+        
+        // Retrieve game iframe
+        var iframe = document.getElementById("game-window");
+        
+        // Set game screen to full screen
+        if (iframe.requestFullscreen) {
+            iframe.requestFullscreen();
+        } else if (iframe.webkitRequestFullscreen) {
+            iframe.webkitRequestFullscreen();
+        } else if (iframe.mozRequestFullScreen) {
+            iframe.mozRequestFullScreen();
+        } else if (iframe.msRequestFullscreen) {
+            iframe.msRequestFullscreen();
+        }
+        
+    }
+}
